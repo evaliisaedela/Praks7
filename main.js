9fe74d9e8f2b228a2229fad25fc58ab1df7ed3da
Eva-Liisa Edela <evaliisae@Eva-Liisas-MacBook-Air.local>
2018-10-10 12:27:06 +0300

    Naelad, untsid lisatud

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var kgInput = document.getElementById('kgInput'); 
 /*toome välja dokumendi object model elemendid*/
 var gramOutput = document.getElementById('gramOutput');
+var poundOutput = document.getElementById('poundOutput');
+var untsOutput = document.getElementById('untsOutput');
 kgInput.addEventListener('input', e /*või event*/ => {
 	var kg = event.target.value;
 	gramOutput.innerHTML = `${kg * 1000} g`;
+	poundOutput.innerHTML = `${kg * 2.2046} lb`;
+	untsOutput.innerHTML = `${kg * 35.2739} oz`;
 }); /*listening to events*/
