09ffeec699360b02ce549b1110638a9c1b1b7ac0
Eva-Liisa Edela <evaliisae@Eva-Liisas-MacBook-Air.local>
2018-10-10 11:35:32 +0300

    Grammide kalkuleerimise võimalus

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var kgInput = document.getElementById('kgInput'); 
 /*toome välja dokumendi object model elemendid*/
+var gramOutput = document.getElementById('gramOutput');
 kgInput.addEventListener('input', e /*või event*/ => {
 	var kg = event.target.value;
-	console.log(kg);
+	gramOutput.innerHTML = `${kg * 1000} g`;
 }); /*listening to events*/
