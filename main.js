fd34041d9a36f19b644fd7112aed521a737242cd
Eva-Liisa Edela <evaliisae@Eva-Liisas-MacBook-Air.local>
2018-10-10 12:42:17 +0300

    Lisasime if else visibility kohta

--- main.js
+++ main.js
@@ -3,9 +3,19 @@ var kgInput = document.getElementById('kgInput');
 var gramOutput = document.getElementById('gramOutput');
 var poundOutput = document.getElementById('poundOutput');
 var untsOutput = document.getElementById('untsOutput');
+var output = document.getElementById('output');
+
+output.style.visibility = 'hidden';
+
+
+/*listening to events*/
 kgInput.addEventListener('input', e /*või event*/ => {
 	var kg = event.target.value;
+	if (kg == 0){
+	output.style.visibility = 'hidden';} else {
+	output.style.visibility = 'visible';	
+	}
 	gramOutput.innerHTML = `${kg * 1000} g`;
 	poundOutput.innerHTML = `${kg * 2.2046} lb`;
 	untsOutput.innerHTML = `${kg * 35.2739} oz`;
-}); /*listening to events*/
+}); 
